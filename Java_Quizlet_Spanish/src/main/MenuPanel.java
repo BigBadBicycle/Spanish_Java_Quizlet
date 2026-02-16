6996b13f3cbba608c28de82e2de05c5966ba8495
santiago <santiago@sreyes>
2026-02-16 16:55:29 -0600

    menu work2.

--- Java_Quizlet_Spanish/src/main/MenuPanel.java
+++ Java_Quizlet_Spanish/src/main/MenuPanel.java
@@ -26,10 +26,10 @@ public class MenuPanel extends JPanel implements ActionListener{
 	
 	MenuPanel(QuizletFrame pFrame){
 		
+		Title = new JLabel();
 		settingsP = new JPanel();
 		startP = new JPanel();
 		verbDefB = new JButton();
-		Title = new JLabel();
 		conjugationB = new JButton();
 		ConjSettingsBs = new JButton[3];
 		
@@ -43,11 +43,26 @@ public class MenuPanel extends JPanel implements ActionListener{
 		conjugationB.setPreferredSize(new Dimension(200,100));
 		verbDefB.setBackground(new Color(246,240,215));
 		conjugationB.setBackground(new Color(246,240,215));
-		
+		verbDefB.setFocusable(false);
+		conjugationB.setFocusable(false);
+		verbDefB.setLabel("Verb Definition");
+		conjugationB.setLabel("Conjugation");
 		
 		settingsP.setPreferredSize(new Dimension(pFrame.getWidth(), 150));
 		settingsP.setBackground(new Color(156, 171, 132));
 		settingsP.setLayout(new FlowLayout());
+		
+		ConjSettingsBs[0] = new JButton();
+		ConjSettingsBs[1] = new JButton();
+		ConjSettingsBs[2] = new JButton();
+		
+		for(int i = 0; i<ConjSettingsBs.length;i++) {
+			ConjSettingsBs[i].setFocusable(false);
+			ConjSettingsBs[i].setFont(new Font("Roboto",Font.BOLD, 20));
+			ConjSettingsBs[i].setBackground(new Color(246,240,215));
+			ConjSettingsBs[i].setPreferredSize(new Dimension(100,50));
+			settingsP.add(ConjSettingsBs[i]);
+		}
 		
 		startP.setBackground(new Color(197, 216, 157));
 		startP.setLayout(new FlowLayout());
@@ -65,6 +80,13 @@ public class MenuPanel extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()== verbDefB) {
+			
+		}
+		if(e.getSource()== conjugationB) {
+			
+		}
 		
 	}
 	
